f222eb988405529dc12d6ec47e9babff4cdfbb41
Ryan Giordano <rgiordan@gmail.com>
2017-02-27 18:30:31 -0800

    bug fix

--- R/InfluenceFunction.R
+++ R/InfluenceFunction.R
@@ -1,81 +1,81 @@
-######################################
-# Influence function helpers
-
-# Get everything necessary for the calculation of influence functions from a draw, u.
-GetInfluenceFunctionSampleFunction <- function(
-  GetLogVariationalDensity,
-  GetLogPrior,
-  GetULogDensity,
-  lrvb_pre_factor) {
-
-  function(u) {
-    log_q_derivs <- GetLogVariationalDensity(u)
-    log_prior_val <- GetLogPrior(u)
-    log_u_density <- GetULogDensity(u)
-
-    # It doesn't seem to matter if I just use the inverse.
-    # lrvb_term <- -1 * lrvb_terms$jac %*% solve(lrvb_terms$elbo_hess, mu_q_derivs$grad)
-    lrvb_term <- lrvb_pre_factor %*% log_q_derivs$grad
-    influence_function <- exp(log_q_derivs$val - log_prior_val) * lrvb_term
-
-    return(list(u=u,
-                lrvb_term=lrvb_term,
-                log_q_val=log_q_derivs$val,
-                log_prior_val=log_prior_val,
-                log_u_density=log_u_density,
-                influence_function=influence_function))
-  }
-}
-
-
-# Use the output of a draw from GetInfluenceFunctionSampleFunction to get sensitivity
-# to a particular contaminating function.
-GetSensitivitySampleFunction <- function(GetLogContaminatingPrior) {
-  function(u_draw) {
-    log_contaminating_prior_val <- GetLogContaminatingPrior(u_draw$u)
-
-    # The vector of sensitivities.
-    log_weight <- log_contaminating_prior_val - u_draw$log_u_density
-    log_influence_factor <- u_draw$log_q_val - u_draw$log_prior_val
-    sensitivity_draw <- exp(log_weight + log_influence_factor) * u_draw$lrvb_term
-
-    # The "mean value theorem" sensitivity
-    prior_val <- exp(u_draw$log_prior_val)
-    contaminating_prior_val <- exp(log_contaminating_prior_val)
-    prior_ratio <- exp(u_draw$log_prior_val - log_contaminating_prior_val)
-
-    mv_sensitivity_draw <-
-      exp(log_influence_factor + u_draw$log_prior_val + log_contaminating_prior_val - u_draw$log_u_density) *
-      (log_contaminating_prior_val - u_draw$log_prior_val) *
-      u_draw$lrvb_term / (contaminating_prior_val - prior_val)
-
-    return(list(sensitivity_draw=sensitivity_draw,
-                mv_sensitivity_draw=mv_sensitivity_draw,
-                log_weight=log_weight,
-                log_influence_factor=log_influence_factor))
-  }
-}
-
-
-# Unpack the output of GetSensitivitySampleFunction
-UnpackSensitivityList <- function(sensitivity_list) {
-    sens_vec_list <- lapply(sensitivity_list, function(entry) { as.numeric(entry$sensitivity_draw) } )
-    sens_vec_list_squared <- lapply(sensitivity_list, function(entry) { as.numeric(entry$sensitivity_draw) ^ 2 } )
-    sens_vec_mean <- Reduce(`+`, sens_vec_list) / n_samples
-    sens_vec_mean_square <- Reduce(`+`, sens_vec_list_squared) / n_samples
-    sens_vec_sd <- sqrt(sens_vec_mean_square - sens_vec_mean^2) / sqrt(n_samples)
-
-    mv_sens_vec_list <- lapply(sensitivity_list, function(entry) { as.numeric(entry$mv_sensitivity_draw) } )
-    mv_sens_vec_list_squared <- lapply(sensitivity_list, function(entry) { as.numeric(entry$mv_sensitivity_draw) ^ 2 } )
-    mv_sens_vec_mean <- Reduce(`+`, mv_sens_vec_list) / n_samples
-    mv_sens_vec_mean_square <- Reduce(`+`, mv_sens_vec_list_squared) / n_samples
-    mv_sens_vec_sd <- sqrt(mv_sens_vec_mean_square - mv_sens_vec_mean^2) / sqrt(n_samples)
-
-    return(list(sens_vec_mean=sens_vec_mean, sens_vec_sd=sens_vec_sd,
-              mv_sens_vec_mean=mv_sens_vec_mean, mv_sens_vec_sd=mv_sens_vec_sd))
-}
-
-
+# ######################################
+# # Influence function helpers
+# 
+# # Get everything necessary for the calculation of influence functions from a draw, u.
+# GetInfluenceFunctionSampleFunction <- function(
+#   GetLogVariationalDensity,
+#   GetLogPrior,
+#   GetULogDensity,
+#   lrvb_pre_factor) {
+# 
+#   function(u) {
+#     log_q_derivs <- GetLogVariationalDensity(u)
+#     log_prior_val <- GetLogPrior(u)
+#     log_u_density <- GetULogDensity(u)
+# 
+#     # It doesn't seem to matter if I just use the inverse.
+#     # lrvb_term <- -1 * lrvb_terms$jac %*% solve(lrvb_terms$elbo_hess, mu_q_derivs$grad)
+#     lrvb_term <- lrvb_pre_factor %*% log_q_derivs$grad
+#     influence_function <- exp(log_q_derivs$val - log_prior_val) * lrvb_term
+# 
+#     return(list(u=u,
+#                 lrvb_term=lrvb_term,
+#                 log_q_val=log_q_derivs$val,
+#                 log_prior_val=log_prior_val,
+#                 log_u_density=log_u_density,
+#                 influence_function=influence_function))
+#   }
+# }
+# 
+# 
+# # Use the output of a draw from GetInfluenceFunctionSampleFunction to get sensitivity
+# # to a particular contaminating function.
+# GetSensitivitySampleFunction <- function(GetLogContaminatingPrior) {
+#   function(u_draw) {
+#     log_contaminating_prior_val <- GetLogContaminatingPrior(u_draw$u)
+# 
+#     # The vector of sensitivities.
+#     log_weight <- log_contaminating_prior_val - u_draw$log_u_density
+#     log_influence_factor <- u_draw$log_q_val - u_draw$log_prior_val
+#     sensitivity_draw <- exp(log_weight + log_influence_factor) * u_draw$lrvb_term
+# 
+#     # The "mean value theorem" sensitivity
+#     prior_val <- exp(u_draw$log_prior_val)
+#     contaminating_prior_val <- exp(log_contaminating_prior_val)
+#     prior_ratio <- exp(u_draw$log_prior_val - log_contaminating_prior_val)
+# 
+#     mv_sensitivity_draw <-
+#       exp(log_influence_factor + u_draw$log_prior_val + log_contaminating_prior_val - u_draw$log_u_density) *
+#       (log_contaminating_prior_val - u_draw$log_prior_val) *
+#       u_draw$lrvb_term / (contaminating_prior_val - prior_val)
+# 
+#     return(list(sensitivity_draw=sensitivity_draw,
+#                 mv_sensitivity_draw=mv_sensitivity_draw,
+#                 log_weight=log_weight,
+#                 log_influence_factor=log_influence_factor))
+#   }
+# }
+# 
+# 
+# # Unpack the output of GetSensitivitySampleFunction
+# UnpackSensitivityList <- function(sensitivity_list) {
+#     sens_vec_list <- lapply(sensitivity_list, function(entry) { as.numeric(entry$sensitivity_draw) } )
+#     sens_vec_list_squared <- lapply(sensitivity_list, function(entry) { as.numeric(entry$sensitivity_draw) ^ 2 } )
+#     sens_vec_mean <- Reduce(`+`, sens_vec_list) / n_samples
+#     sens_vec_mean_square <- Reduce(`+`, sens_vec_list_squared) / n_samples
+#     sens_vec_sd <- sqrt(sens_vec_mean_square - sens_vec_mean^2) / sqrt(n_samples)
+# 
+#     mv_sens_vec_list <- lapply(sensitivity_list, function(entry) { as.numeric(entry$mv_sensitivity_draw) } )
+#     mv_sens_vec_list_squared <- lapply(sensitivity_list, function(entry) { as.numeric(entry$mv_sensitivity_draw) ^ 2 } )
+#     mv_sens_vec_mean <- Reduce(`+`, mv_sens_vec_list) / n_samples
+#     mv_sens_vec_mean_square <- Reduce(`+`, mv_sens_vec_list_squared) / n_samples
+#     mv_sens_vec_sd <- sqrt(mv_sens_vec_mean_square - mv_sens_vec_mean^2) / sqrt(n_samples)
+# 
+#     return(list(sens_vec_mean=sens_vec_mean, sens_vec_sd=sens_vec_sd,
+#               mv_sens_vec_mean=mv_sens_vec_mean, mv_sens_vec_sd=mv_sens_vec_sd))
+# }
+# 
+# 
 
 ####################
 # New versions:
@@ -140,7 +140,7 @@ GetMCMCInfluenceFunctions <- function(param_draws, GetLogPrior) {
     } else {
         l2_var <- l2_neg_var
     }
-    pos_sign <- 2 * pos_worse[ind] - 1
+    pos_sign <- 2 * pos_worse - 1
     worst_u <-
         exp(log_prior) * abs(mcmc_influence) * (pos_sign * mcmc_influence >= 0) / worst_case
 
@@ -148,13 +148,6 @@ GetMCMCInfluenceFunctions <- function(param_draws, GetLogPrior) {
   }
 
   GetMCMCWorstCase <- function(g_draws) {
-    # mcmc_influence <- GetMCMCInfluence(g_draws)
-    # mcmc_influence_pos <- mcmc_influence * (mcmc_influence > 0)
-    # mcmc_influence_neg <- -1 * mcmc_influence * (mcmc_influence < 0)
-    # mcmc_wc <- max(
-    #   sqrt(mean((mcmc_influence_pos^2) * mcmc_importance_ratio)),
-    #   sqrt(mean((mcmc_influence_neg^2) * mcmc_importance_ratio)))
-    # return(mcmc_wc)  
     return(GetMCMCWorstCaseResults(g_draws)$worst_case)
   }
 
@@ -162,7 +155,9 @@ GetMCMCInfluenceFunctions <- function(param_draws, GetLogPrior) {
               GetMCMCWorstCase=GetMCMCWorstCase,
               GetMCMCWorstCaseResults=GetMCMCWorstCaseResults,
               GetConditionalMeanDiff=GetConditionalMeanDiff,
-              dens_at_draws=dens_at_draws))
+              param_draws=param_draws,
+              dens_at_draws=dens_at_draws$y,
+              log_prior_at_draws=log_prior))
 }
 
 
@@ -208,13 +203,13 @@ GetVariationalInfluenceResults <- function(
   # Get the worst-case perturbation and its magnitude.
   pos_worse <- u_influence_mat_pos_norm > u_influence_mat_neg_norm
   worst_case <- ifelse(pos_worse, u_influence_mat_pos_norm, u_influence_mat_neg_norm)
-  worst_case_u <-
-    sapply(1:ncol(influence_fun),
-           function(ind) {
-             # Choose the positive or negative part according to pos_worse
-             pos_sign <- 2 * pos_worse[ind] - 1
-             exp(log_prior) * abs(influence_fun[, ind]) * (pos_sign * influence_fun[, ind] >= 0) / worst_case[ind]
-             })
+  GetWorstCasePerturbationColumn <- function(ind) {
+      # Choose the positive or negative part according to pos_worse
+      pos_sign <- 2 * pos_worse[ind] - 1
+      exp(log_prior) * abs(influence_fun[, ind]) *
+        (pos_sign * influence_fun[, ind] >= 0) / worst_case[ind]      
+  }
+  worst_case_u <- sapply(1:ncol(influence_fun), GetWorstCasePerturbationColumn)
   
   return(list(
     u_draws=u_draws,
